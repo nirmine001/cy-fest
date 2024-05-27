473ace549ee9464a0e1a73dc70aadb35c8c5b743
elsw3 <152193357+elsw3@users.noreply.github.com>
2024-05-27 02:22:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include "modefestivalgoer.h"
-#include "couleurs.h"  
+#include "couleurs.h" 
 
 
 int main (){
@@ -8,47 +8,45 @@ int main (){
     int nbrh;
     Concert *h = NULL;
 
-    while (1){ //loop that rotates indefinitely. Expression 1 is always true
-        do{
-        printf("choose the mode : (1 for MANAGER, 2 for FESTIVAL-GOER, 3 to Exit): ");
-        scanf (" %c", &mode); 
-        }while (mode != '1' && mode != '2' && mode !='3');
-        
-        if (mode == '3'){
-            break; 
-        }	
-        else if (mode == '1'){
-            printf("\nYou are in MANAGER mode.\n");
-            printf("\nHow many concert halls do you want to create for the festival?: ");
-            scanf("%d",&nbrh);
-            hall = malloc(nbrh * sizeof(ConcertHall));
-            if (hall == NULL) {
-                printf("Memory allocation problem! \n");
-                exit(1);
-            }
-             for (int i = 0; i< nbrh; i++) {
-                printCREATION_HALL(&hall[i]);
-            }
-            printf("\nHow many concert do you want to create for the festival?: ");
-            scanf("%d",&nbrh);
-            h = malloc(nbrh * sizeof(Concert));
-            if (h == NULL) {
-                printf("Memory allocation problem! \n");
-                exit(1);
-            }
-            for (int i = 0; i< nbrh; i++) {
-                printCREATION_CONCERT(&h[i]);
-            }
-            
-        }
-        else if (mode == '2'){ 
-            void printfMODEFESTIVALIER(nbrh);
-        } 
+    while (1) { //loop that rotates indefinitely. Expression 1 is always true
+        do {
+            printf("choose the mode : (1 for MANAGER, 2 for FESTIVAL-GOER, 3 to Exit): ");
+            scanf("%c", &mode);  
 
-        else{
-        printf("Error in your choice. Please choose again.\n");
-        mode=0;
-    }
+            if (mode == '3') {
+                break; // quitter programme
+            }	
+            else if (mode == '1') {
+                printf("\nYou are in MANAGER mode.\n");
+                printf("\nHow many concert halls do you want to create for the festival?: ");
+                scanf("%d", &nbrh);
+                hall = malloc(nbrh * sizeof(ConcertHall));
+                if (hall == NULL) {
+                    printf("Memory allocation problem! \n");
+                    exit(1);
+                }
+                for (int i = 0; i < nbrh; i++) {
+                    printCREATION_HALL(&hall[i]);
+                }
+                printf("\nHow many concert do you want to create for the festival?: ");
+                scanf("%d", &nbrh);
+                h = malloc(nbrh * sizeof(Concert));
+                if (h == NULL) {
+                    printf("Memory allocation problem! \n");
+                    exit(1);
+                }
+                for (int i = 0; i < nbrh; i++) {
+                    printCREATION_CONCERT(&h[i]);
+                }
+            }
+            else if (mode =='2') {
+                printf("\nYou are in FESTIVALGOER mode.\n");
+                printfMODEFESTIVALGOER(nbrh); 
+            } else {
+                printf("Error in your choice. Please choose again.\n");
+                mode = 0;
+            }
+        } while (mode != '1' && mode != '2' && mode != '3');
     }
     
     if (hall) {
@@ -63,4 +61,3 @@ int main (){
 
  return 0;
 }
-
